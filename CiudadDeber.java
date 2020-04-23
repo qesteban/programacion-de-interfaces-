9f7b48d1826e12d8e911ba10e84a7985a4582912
qesteban <64221459+qesteban@users.noreply.github.com>
2020-04-23 12:55:20 -0500

    Add files via upload
    deber programacion de interfaces

---
+++ CiudadDeber.java
@@ -0,0 +1,30 @@
+public class CiudadDeber {    //UpperCamelCase
+    public static void main(String[] args) {
+
+        //declaracion de variables
+        //declaracion
+        int poblacion = 300; //asignacion   //LowerCamelCase
+
+        char genero = 'M';                  //LowerCamelCase
+        char genero1 = 'F';
+
+
+        String nombreCiudad = "Ecuador";
+        boolean paisConCoronaVirus = false;
+                paisConCoronaVirus = true;
+
+        byte edad = '4';
+
+        String numeroDePoblacion = "150";
+
+
+
+        System.out.println( "En la ciudad " +nombreCiudad+ " Tiene una poblacion de " +poblacion+ " con numero de poblacion de " +numeroDePoblacion+ " de genero " +genero+ " y " +numeroDePoblacion+ " de genero " +genero1+ " con una edad promedio de " +edad+
+                " y el el test del pais en resultado con coronavirus es " +paisConCoronaVirus );
+
+
+    }
+
+
+
+}
